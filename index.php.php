6314d8ebbe5b61f539171b503b68905e9a34cd4e
Pedro Xavier <pedrohxavier70@gmail.com>
2022-08-14 11:48:55 -0300

    mudei o primeiro echo e adicionei o ola udemy

--- index.php.php
+++ index.php.php
@@ -1,12 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-   echo "Olá Mundo456";
-=======
    echo "Olá Mundo123!";
 
-   echo "Olá Hcode";
->>>>>>> 9b80bc193dba6a7b3f95fcc29719851c1bb0443c
+   echo "Olá Hcode e Udemy";
 
-   echo "Olá Udemy!";
 ?>
